b8f10b1f4fc1bf4d0e7c69c73aad8beed53b751d
Jakob Rockenbauch <jakob.rockenbauch@snabble.io>
2019-05-23 13:26:50 +0200

    Add some comments

--- model/koordinaten.go
+++ model/koordinaten.go
@@ -9,16 +9,19 @@ type Koordinate struct {
 	Y float64
 }
 
+// Add addiert zwei Koordinaten
 func (k Koordinate) Add(o Koordinate) Koordinate {
 	k.X += o.X
 	k.Y += o.Y
 	return k
 }
 
+// Inverse gibt das additive Inverse der Koordinate
 func (k Koordinate) Inverse() Koordinate {
 	return k.Multiply(-1)
 }
 
+// Length berechnet die Länge der Koordinate
 func (k Koordinate) Length() float64 {
 	l := math.Sqrt(k.X*k.X + k.Y*k.Y)
 	if math.IsNaN(l) {
@@ -27,10 +30,12 @@ func (k Koordinate) Length() float64 {
 	return l
 }
 
+// Abstand berechnet den Abstand zwischen zwei Koordinaten
 func (k Koordinate) Abstand(o Koordinate) float64 {
 	return k.Add(o.Inverse()).Length()
 }
 
+// Normalize normiert die Koordinate auf die Länge 1
 func (k Koordinate) Normalize() Koordinate {
 	l := k.Length()
 	if l == 0 {
@@ -39,6 +44,7 @@ func (k Koordinate) Normalize() Koordinate {
 	return k.Multiply(1. / l)
 }
 
+// Multiply streckt die Koordinate um Faktor c
 func (k Koordinate) Multiply(c float64) Koordinate {
 	x := k.X * c
 	y := k.Y * c
@@ -51,7 +57,7 @@ func (k Koordinate) Multiply(c float64) Koordinate {
 	return Koordinate{x, y}
 }
 
-// Normalisierter Richtungsvektor von der Koordinate in Richtung o
+//Richtung berechnet den normalisierten Richtungsvektor von der Koordinate in Richtung Koordinate o
 func (k Koordinate) Richtung(o Koordinate) Koordinate {
 	return o.Add(k.Inverse()).Normalize()
 }
